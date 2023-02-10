2c77a1f8a6d7c0d7fbfde22d5e2c1fa98a14bd8f
Nikhil Upadhyay <nikhil.upadhyay@quest-global.com>
2023-02-10 14:09:39 +0530

    first

---
+++ src/main/java/com/quest/crud/app/controller/EmployeeController.java
@@ -0,0 +1,74 @@
+package com.quest.crud.app.controller;
+
+import com.quest.crud.app.exception.ResourceNotFoundException;
+import com.quest.crud.app.model.Employee;
+import com.quest.crud.app.repository.EmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    @Autowired
+    private EmRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+
+        return employeeRepository.findAll();
+    }
+
+    // build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+       // User sevedUser=service.save(user);
+
+        Employee userSave = employeeRepository.save(employee);
+
+       //  return employeeRepository.save(employee);
+
+        return  userSave;
+    }
+
+    // build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable  long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // build update employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,@RequestBody Employee employeeDetails) {
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+    // build delete employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        employeeRepository.delete(employee);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+}
